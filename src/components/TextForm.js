accb6e845c1cef170ef7c6d57886134fd1258858
urvi004 <urvisharma54@gmail.com>
2022-06-21 11:03:37 +0530

    functionality added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,6 +12,12 @@ export default function TextForm(props) {
     setText(newText)
   }
 
+  const handleLowClick = () => {
+    console.log("lowerCase was clicked")
+    let newText = text.toLowerCase();
+    setText(newText)
+  }
+
   const handleOnChange = (e) => {
     console.log("on Change")
     setText(e.target.value)
@@ -19,13 +25,23 @@ export default function TextForm(props) {
 
 
   return (
-    <div>
+    <>
+    <div className= "container">
         <h1>{props.heading}</h1>
       <div className="mb-3">
          <label htmlFor="myBox" className="form-label"></label>
          <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick} >Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick} >Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick} >Convert to LowerCase</button>
     </div>
+    <div className="container my-3">
+         <h2>your text summary</h2>
+         <p> {text.split(" ").length} words and {text.length} character</p>  
+         <p> {0.08*text.split(" ").length} Minutes read</p> 
+         <h2>Preview</h2>
+         <p>{text}</p>  
+    </div>
+    </>
   )
 }
