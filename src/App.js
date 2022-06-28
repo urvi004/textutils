2a8965ed7a417e8fe10a65b13f2c22c60cae55ad
urvi004 <urvisharma54@gmail.com>
2022-06-28 22:24:32 +0530

    Route added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import Alert from './components/Alert'
+import About from './components/About'; 
+import Alert from './components/Alert';
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
 
@@ -24,23 +30,33 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = '#042743';
       showAlert("dark mode has been enabled", "success");
+      document.title = "TextUtils- Dark Mode"
     }
     else{
       setMode("light");
       document.body.style.backgroundColor = 'white';
       showAlert("light mode has been enabled", "success");
+      document.title = "TextUtils- Light Mode"
     }
   }
   
     return (
-    <>
+    
+    <Router>
+      
       <Navbar title = "textUtils" mode= {mode} toggleMode={toggleMode}/>
       <Alert  alert= {alert}/>
       <div className="container my-3">
-      <TextForm showAlert= {showAlert} heading ="Enter text to analyze" mode= {mode}/> 
+      <Routes>
+          <Route path = "/about"
+                 element= {<About/>}/>
+          <Route path = "/" 
+                 element= { <TextForm showAlert= {showAlert} heading ="Enter text to analyze" mode= {mode}/> }/>
+                 </Routes>
       </div>
-     
-    </>
+      
+      </Router>
+    
   );
 }
 
